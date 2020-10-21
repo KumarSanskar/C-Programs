2aecead3383f23187749e39742005742b4aa5e3e
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-10-21 19:50:00 +0530

    to add two numbers and display their sum

---
+++ Sum.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_num = 10; //declaring and initialising variables to store values. 
+    int second_num = 40;
+
+    int sum = first_num + second_num;   //performing operation and storing value in third variable.
+    cout << "sum of two numbers is: "<<first_num << "+"<< second_num << " = "<<sum;  
+}
