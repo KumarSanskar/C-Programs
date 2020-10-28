8a61bd897eadc9ce4bc395077b69849e01158f63
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2020-10-28 13:33:09 +0530

    takes input from user and addss them.

---
+++ SumByTakingInput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int firstNumber,secondNumber,sum;
+    cout<< "Enter first number: ";
+    cin>> firstNumber;
+    cout<<"Enter second number: ";
+    cin>>secondNumber;
+
+    sum = firstNumber + secondNumber;
+    cout<<"sum of "<<firstNumber <<"+"<<secondNumber<<"="<<sum;
+
+
+}
